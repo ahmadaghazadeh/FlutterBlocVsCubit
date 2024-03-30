8aa6e420ddbf87005cb1f06c9d09290817202511
ahmad.aghazadeh <Ahmad.aghazadeh.a@gmail.com>
2024-03-30 16:43:57 +0330

    Understand Cubit

--- flutter_pizza_bloc/lib/bloc/counter_bloc.dart
+++ flutter_pizza_bloc/lib/bloc/counter_bloc.dart
@@ -4,14 +4,14 @@ part 'counter_event.dart';
 class CounterBloc extends Bloc<CounterEvent, int> {
   CounterBloc() : super(0) {
     on<CounterIncremented>((event, emit) {
-      emit(state + 1);
+      emit(state + 2);
     });
 
     on<CounterDecremented>((event, emit) {
       if (state == 0) {
         return;
       }
-      emit(state - 1);
+      emit(state - 2);
     });
   }
 }
--- flutter_pizza_bloc/lib/home_page.dart
+++ flutter_pizza_bloc/lib/home_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pizza_bloc/bloc/counter_bloc.dart';
 import 'package:flutter_pizza_bloc/cubit/counter_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_pizza_bloc/cubit/inc_dec_cubit.dart';
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+import 'package:flutter_pizza_bloc/inc_dec_page.dart';
+class MyHomePage  extends StatelessWidget {
+  const MyHomePage ({super.key});
   
   @override
   Widget build(BuildContext context) {
@@ -13,48 +13,68 @@ class MyApp extends StatelessWidget {
         child: MaterialApp(
             title: 'Flutter Demo',
             theme: ThemeData(
-      
               colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
               useMaterial3: true,
             ),
-            home: const MyHomePage(title: 'Flutter Demo Home Page'),
+            home: const HomePage(title: 'Flutter Demo Home Page'),
           ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key, required this.title});
   final String title;
 
   @override
   Widget build(BuildContext context) {
+        final counterCubit = BlocProvider.of<CounterCubit>(context);
+        final counterBloc = BlocProvider.of<CounterBloc>(context);
         return Scaffold(
           appBar: AppBar(
             backgroundColor: Theme.of(context).colorScheme.inversePrimary,
             title: Text(title),
           ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'Cubit Counter:',
+                ),
+                BlocBuilder<CounterCubit,int>(
+                  builder: (context,counter) {
+                  return Text(
+                    '$counter',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                }),
+                const Text(
+                  'Bloc Counter:',
+                ),
+                BlocBuilder<CounterBloc,int>(
+                  builder: (context,counter) {
+                  return Text(
+                    '$counter',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                }),
+              ],
+            ),
+          ),
           floatingActionButton: Column(
             mainAxisAlignment:MainAxisAlignment.end ,
             children: [
+             
               FloatingActionButton(
-                onPressed: () {
-                  Navigator.of(context).push(
+                onPressed: (){
+                   Navigator.of(context).push(
                     MaterialPageRoute(
                       builder: (context) => const IncDecPage(),
                     ),
                   );
                 },
-                child: const Icon(Icons.navigate_next),
-              ),
-              FloatingActionButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => const IncDecPage(),
-                    ),
-                  );
-                },
+                tooltip: 'Decrement',
                 child: const Icon(Icons.navigate_next),
               ),
             ],
